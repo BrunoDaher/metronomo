276659f863874f2ff141e334750412d39f5958cc
brunus9 <brunus9@gmail.com>
2025-01-23 23:05:44 -0300

    teste

--- js/metronomo.js
+++ js/metronomo.js
@@ -82,7 +82,7 @@ function start(){
     play();
         
     function play(){
-     playNote(i==1?'100':'110','sine',i==1?'160':'30');   
+     playNote(i==1?'140':'280','sine',i==1?'160':'230');   
     }
     
     fig();        
